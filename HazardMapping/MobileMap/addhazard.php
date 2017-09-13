555267d788ebed7af1930f06aa28cd91875f2a43
Luqmaan Hassim <luqmaanhassim1@gmail.com>
2017-09-14 01:05:37 +0200

    Add files via upload

---
+++ HazardMapping/MobileMap/addhazard.php
@@ -0,0 +1,28 @@
+<?php
+	session_start();
+	$host        = "host=url";
+	$port        = "port=5432";
+	$dbname      = "dbname=devgroup";
+	$credentials = "user=user password=password;
+
+	$db = pg_connect("$host $port $dbname $credentials");
+
+	$varlat = pg_escape_string($_POST['lat']);
+	$varlng = pg_escape_string($_POST['lng']);
+	$ecplus_fie = pg_escape_string($_POST['ecplus_fie']);
+
+	$query1 = "INSERT INTO hazards(ecplus_fie, fieldwor_8, fieldwor_9, geom) VALUES ('$ecplus_fie', '$varlat', '$varlng', ST_GeomFromText('POINT($varlng $varlat)', 4326))";
+	//$query1 = "INSERT INTO hazards(ecplus_fie, fieldwor_8, fieldwor_9, geom) VALUES ('$ecplus_fie', '$varlat', '$varlng', ST_SetSrid(ST_MakePoint('$varlng', '$varlat'), 32735));
+	$result = pg_query($query1);
+  if (!$result) {
+  		$errormessage = pg_last_error();
+      echo $query1;
+      exit();
+  }
+  printf ("These values were inserted into the database - %s %s %s", $ecplus_fie, $varlat, $varlng);
+  pg_close();
+
+	header("Location: index.html");
+	exit;
+
+?>
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HazardMapping
-Mapping application that allows users to report hazards.
+A simple mobile and desktop web app that allows residents of informal settlements to report hazards
+
+General Interface
+![General Interface](https://i.imgur.com/kXFihu0.png "Interface")
+
+Right click to open a context menu (Leaflet-Context Menu) and add a hazard to run the php entry
+![Entry Section](https://i.imgur.com/kF1XBYc.png "Add a Hazard")
